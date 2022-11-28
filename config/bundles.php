32d87179fb20bb429c413f0e308c97fbd7da7fbb
fabien Vernières <fabienvernieres@gmail.com>
2022-11-28 17:57:37 +0100

    uninstall encore

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
